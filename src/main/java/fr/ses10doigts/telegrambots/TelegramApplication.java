669f8742d6b79829fb7e50d6495b9362014b7abb
oKlm <df4ze31@gmail.com>
2026-03-20 22:36:59 +0100

    Packaging for module

--- src/main/java/fr/ses10doigts/telegrambots/TelegramApplication.java
+++ src/main/java/fr/ses10doigts/telegrambots/TelegramApplication.java
@@ -1,26 +1,13 @@
 package fr.ses10doigts.telegrambots;
 
-import fr.ses10doigts.telegrambots.service.sender.TelegramSender;
-import fr.ses10doigts.telegrambots.service.sender.TelegramSenderRegistry;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
-public class TelegramApplication implements CommandLineRunner {
-    @Autowired
-    private TelegramSenderRegistry telegramSenderRegistry;
+public class TelegramApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(TelegramApplication.class, args);
     }
 
-    public void run(String... args){ // TODO remove with CommandLineRunner implements
-        log.info("call is called");
-        TelegramSender sender = telegramSenderRegistry.getRequiredSender("main-bot");
-        sender.sendMessage(1595302518L, "Qu'allons nous faire ce soir?");
-    }
 }
